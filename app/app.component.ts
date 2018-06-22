12199b6d0b2760c41747fcc136212bbb0d06969d
ImVarunP <40432383+ImVarunP@users.noreply.github.com>
2018-06-22 19:08:50 +0530

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div style="text-align:center">
+  <h1>
+    Marvellous Infosystems!
+  </h1>
+  <h2>
+    Educating for better Tomorrow.
+  </h2>
+  </div>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '';
+}
